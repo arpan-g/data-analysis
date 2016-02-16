6779304c766b9d09610230d9f67c68195b3e646b
arpan-g <A.Govindaraju@student.tudelft.nl>
2016-02-16 11:47:24 +0100

    16th data added

--- time_analysis.m
+++ time_analysis.m
@@ -7,7 +7,7 @@ UINT8_MAX_NUM = 256;
 % listing=dir(nameOfDir);
 % matFile = strcat(nameOfDir,'\',listing(3).name);
 % load(matFile);
-load('packet_loss_15_02_17_34.mat');
+load('packet_loss_16_02_morning.mat');
 recPAckets=ones(1,8);
 length = size(dataArray);
 % min([dataArrat()])
