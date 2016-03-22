ce3854c47c6c7d9695ff6d4c84acca253e94c309
Govindaraju <arpan.govindaraju@philips.com>
2016-03-22 15:37:01 +0100

    changes on 22

--- clustering_kmean.m
+++ clustering_kmean.m
@@ -2,12 +2,12 @@
 %correlation matrix obtained by correlating the energy matrix
 % First step would be to remove the 1's for the same nodes it will
 % mislead the kmean algorithm.
-% clear 
+% clear
 optimistic_neighbouring_map=...
-  [ 0     1     1    -1     0     0     0     0;...
+    [ 0     1     1    -1     0     0     0     0;...
     1     0    -1     1     0     0     0     0;...
     1    -1     0     1     1    -1     0     0;...
-   -1     1     1     0    -1     1     0     0;...
+    -1     1     1     0    -1     1     0     0;...
     0     0     1    -1     0     1     1    -1;...
     0     0    -1     1     1     0    -1     1;...
     0     0     0     0     1    -1     0     1;...
@@ -22,12 +22,18 @@ for i = 1:8
     end
 end
 
-IDX=kmeans(correlation',2);
+[IDX,C]=kmeans(correlation',2);
+if(C(2)>C(1))
+    IDX=IDX-1;
+else
+    IDX=IDX-2;
+    IDX=abs(IDX);
+end
 count = 1;
 for i = 1:8
     for j = i+1:8
         newGraph(i,j)=IDX(count)-1;
-%           graphMatrix=abs(graphMatrix);
+        %           graphMatrix=abs(graphMatrix);
         count = count + 1;
     end
 end
@@ -35,29 +41,29 @@ end
 
 % for i = 1:8
 %     for j = 1:8
-        graphMatrix=newGraph+newGraph';
-        falsepositive=0;
-    for i= 1:8
-        for j = i+1:8
-            if(optimistic_neighbouring_map(i,j)==0)
-                if( optimistic_neighbouring_map(i,j)~=   graphMatrix(i,j));
-                    falsepositive=falsepositive+1;
-                end
+graphMatrix=newGraph+newGraph';
+falsepositive=0;
+for i= 1:8
+    for j = i+1:8
+        if(optimistic_neighbouring_map(i,j)==0)
+            if( optimistic_neighbouring_map(i,j)~=   graphMatrix(i,j));
+                falsepositive=falsepositive+1;
             end
         end
     end
-    falseNegative=0;
-    for i= 1:8
-        for j = i+1:8
-            if(optimistic_neighbouring_map(i,j)==1)
-                if( optimistic_neighbouring_map(i,j)~=   graphMatrix(i,j));
-                    falseNegative=falseNegative+1;
-                    
-                end
+end
+falseNegative=0;
+for i= 1:8
+    for j = i+1:8
+        if(optimistic_neighbouring_map(i,j)==1)
+            if( optimistic_neighbouring_map(i,j)~=   graphMatrix(i,j));
+                falseNegative=falseNegative+1;
+                
             end
         end
     end
-    te=falseNegative/16+falsepositive/12;
+end
+te=falseNegative/16+falsepositive/12;
 % G= graph(newGraph);
 % plot(G);
 
--- time_v_error_script.m
+++ time_v_error_script.m
@@ -2,7 +2,7 @@ clear all
 counter=1;
 for time_period =36000:36000:2007539
 clearvars -except newPirArray evaluation_matrix counter time_period
-load('combined_data\combi_10.mat');
+load('combined_data\combi.mat');
 power_consumption
 clustering_kmean
 evaluation_matrix(counter,:)=[time_period,falseNegative,falsepositive,falseNegative/16+falsepositive/12];
